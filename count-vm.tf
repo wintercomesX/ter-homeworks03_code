5d190c704faa86bdf2be10cec6bfc10247a68b16
wintercomesX <149241202+wintercomesX@users.noreply.github.com>
2024-12-13 12:13:36 +0300

    Update count-vm.tf

--- count-vm.tf
+++ count-vm.tf
@@ -25,7 +25,7 @@ resource "yandex_compute_instance" "web" {
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
-    security_group_ids = ["enpfjh55d01i8l6n9l0p"]
+    security_group_ids = [var.security_id]
   }
 
   metadata = {
